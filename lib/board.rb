e80ed2bfed7d15e7a9ead8aa82ede23e0bb57cc6
giuseppe de santis <giusdesan@gmail.com>
2015-07-30 13:55:18 +0100

    added method to define coordinates on board and passing tests

--- lib/board.rb
+++ lib/board.rb
@@ -2,4 +2,23 @@ require_relative 'ship'
 
 class Board
 
+  def define_coordinates(ship, cell, direction)
+    coordinates = [cell]
+    x = cell[1]
+    y = cell[0]
+    i = 1
+    if direction == 'H'
+      (ship.size-1).times do
+        coordinates << [y, (x+i)]
+        i+=1
+      end
+    elsif direction == 'V'
+      (ship.size-1).times do
+        coordinates << [(y+i), x]
+        i+=1
+      end
+    end
+    coordinates
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,11 @@
 require_relative '../lib/board'
 
 describe Board do
+  let(:ship) { double(:ship, :size => 2) }
 
-  it 'can place a ship' do
+  it 'can define coordinates when given a ship, starting cell and direction' do
+    cell = [0, 0]
+    expect(subject.define_coordinates(ship, cell, 'V')).to eq [[0,0], [1,0]]
   end
 
 end
